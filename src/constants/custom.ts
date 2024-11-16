5d9b4d7263caa1444519ce66c6082d308a6a34ec
abdur-raqeeb ajao <ajaorqb@gmail.com>
2024-11-15 19:15:37 -0700

    chore: update reply and no_reply constant

--- src/constants/custom.ts
+++ src/constants/custom.ts
@@ -1,5 +1,5 @@
 export const DEFAULT_SIZE = 20;
 export const SALT_VALUE = 10;
 export const PASSWORD_REGEX = '^(?=.*?[A-Z])(?=(.*[a-z]){1,})(?=(.*[\\d]){1,})(?=(.*[\\W]){1,})(?!.*\\s).{8,}$';
-export const NO_REPLY = 'no-reply@alifhub.co';
-export const REPLY_TO = 'reply-to@alifhub.co';
+export const NO_REPLY = 'no-reply@custom.co';
+export const REPLY_TO = 'reply-to@custom.co';
